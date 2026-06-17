ff442fa1a030889847140a1843975b1d52eea158
Federico Mangini <federicomangini10@gmail.com>
2026-06-17 20:23:43 +0200

    Aggiornamento pacchetto NuGet con spostamento su NuGet.org per pubblicazione pacchetti

---
+++ BlazorFeatures.Abstractions/PagedRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorFeatures.Abstractions
+{
+    public interface IPagedRequest
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string? OrderBy { get; set; }
+    }
+
+    public class PagedRequest : IPagedRequest
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string? OrderBy { get; set; }
+    }
+}
---
+++ BlazorFeatures.Abstractions/PagedResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorFeatures.Abstractions
+{
+    public interface IPagedResponse<T>
+    {
+        public List<T> Items { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+
+    public class PagedResponse<T> : IPagedResponse<T>
+    {
+        public List<T> Items { get; set; } = [];
+
+        public int TotalCount { get; set; }
+    }
+}
